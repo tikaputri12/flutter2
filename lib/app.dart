fefc9b80c93632d4b21f19023288bfd452b2b868
tika kartika <tikaputri469@gmail.com>
2026-05-22 20:06:12 +0700

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -7,11 +7,14 @@ import 'package:flutter2/login/bloc/login_bloc.dart';
 import 'package:flutter2/login/repository/login_repository.dart';
 import 'package:flutter2/login/view/login_page.dart';
 import 'package:flutter2/posts/bloc/post_bloc.dart';
+import 'package:flutter2/posts/bloc/post_event.dart';
 import 'package:flutter2/posts/view/posts_page.dart';
+import 'package:flutter2/register/bloc/register_bloc.dart';
+import 'package:flutter2/register/repository/register_repository.dart';
+import 'package:flutter2/register/view/register_page.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:http/http.dart' as http;
 
-import 'posts/bloc/post_event.dart';
+import 'package:http/http.dart' as http;
 
 class CounterApp extends StatelessWidget {
   const CounterApp({super.key});
@@ -20,31 +23,82 @@ class CounterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
       providers: [
-        RepositoryProvider(create: (context) => LoginRepository(httpClient: http.Client())),
+
+        // LOGIN REPOSITORY
+        RepositoryProvider(
+          create: (_) => LoginRepository(
+            httpClient: http.Client(),
+          ),
+        ),
+
+        // REGISTER REPOSITORY
+        RepositoryProvider(
+          create: (_) => RegisterRepository(
+            httpClient: http.Client(),
+          ),
+        ),
       ],
+
       child: MultiBlocProvider(
         providers: [
-          BlocProvider(create: (_) => CounterCubit()),
+
+          // COUNTER
           BlocProvider(
-            create: (_) =>
-                PostBloc(httpClient: http.Client())..add(PostFetched()),
+            create: (_) => CounterCubit(),
           ),
-          BlocProvider(create: (_) => ThemeCubit()),
-          BlocProvider(create: (context) => LoginBloc(loginRepository: context.read<LoginRepository>())),
+
+          // POSTS
+          BlocProvider(
+            create: (_) => PostBloc(
+              httpClient: http.Client(),
+            )..add(PostFetched()),
+          ),
+
+          // THEME
+          BlocProvider(
+            create: (_) => ThemeCubit(),
+          ),
+
+          // LOGIN BLOC
+          BlocProvider(
+            create: (context) => LoginBloc(
+              loginRepository:
+                  context.read<LoginRepository>(),
+            ),
+          ),
+
+          // REGISTER BLOC
+          BlocProvider(
+            create: (context) => RegisterBloc(
+              registerRepository:
+                  context.read<RegisterRepository>(),
+            ),
+          ),
         ],
+
         child: BlocBuilder<ThemeCubit, ThemeMode>(
           builder: (context, state) {
             return MaterialApp(
+              debugShowCheckedModeBanner: false,
+
               theme: ThemeData.light(),
               darkTheme: ThemeData.dark(),
-              themeMode: context.watch<ThemeCubit>().state,
-              debugShowCheckedModeBanner: false,
+
+              themeMode:
+                  context.watch<ThemeCubit>().state,
+
               initialRoute: '/login',
+
               routes: {
                 '/home': (_) => const HomePage(),
+
                 '/counter': (_) => const CounterPage(),
+
                 '/posts': (_) => const PostsPage(),
+
                 '/login': (_) => const LoginPage(),
+
+                '/register': (_) => const RegisterPage(),
               },
             );
           },
