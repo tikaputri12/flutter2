36aae8ab00ccaf7058a01c0d73e9604ab9c17ab6
tika kartika <tikaputri469@gmail.com>
2026-05-22 20:06:49 +0700

    Create register_page.dart

---
+++ lib/register/view/register_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter2/register/view/register_view.dart';
+
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const RegisterView();
+  }
+}
