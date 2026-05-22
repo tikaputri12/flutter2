d1e1c1458dd5848fabacfc2bd4053aab2ac54695
tika kartika <tikaputri469@gmail.com>
2026-05-22 20:06:44 +0700

    Create register_view.dart

---
+++ lib/register/view/register_view.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter2/register/bloc/register_bloc.dart';
+import 'package:flutter2/register/bloc/register_event.dart';
+import 'package:flutter2/register/bloc/register_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  final TextEditingController _username = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  final TextEditingController _email = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<RegisterBloc, RegisterState>(
+      listener: (context, state) {
+        // berhasil register
+        if (state is RegisterSuccess) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(state.message)));
+
+          Future.delayed(Duration(seconds: 1), () {
+            Navigator.pushReplacementNamed(context, '/login');
+          });
+        }
+
+        // gagal register
+        if (state is RegisterFailure) {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text("Register Failed"),
+              content: Text(state.message),
+              actions: [
+                TextButton(
+                  child: Text("OK"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          );
+        }
+      },
+      builder: (context, state) {
+        if (state is RegisterLoading) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        return Scaffold(
+          body: SafeArea(
+            child: Center(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Register Page",
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    SizedBox(height: 32.0),
+                    TextField(
+                      controller: _email,
+                      decoration: const InputDecoration(
+                        labelText: "Email",
+                        hintText: "Input Email",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    TextField(
+                      controller: _username,
+                      decoration: const InputDecoration(
+                        labelText: "Username",
+                        hintText: "Input username",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    TextField(
+                      controller: _password,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: "Password",
+                        hintText: "Input Password",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    SizedBox(
+                      height: 50,
+                      width: 200,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          context.read<RegisterBloc>().add(
+                            RegisterSubmitted(
+                              email: _email.text,
+                              username: _username.text,
+                              password: _password.text,
+                            ),
+                          );
+                        },
+                        child: Text("Register"),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
