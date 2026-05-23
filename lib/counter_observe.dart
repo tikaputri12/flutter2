7a9ec3929f4455b3049eb6dc7246385096afc33b
tika kartika <tikaputri469@gmail.com>
2026-05-23 10:55:40 +0700

    Update counter_observe.dart

--- lib/counter_observe.dart
+++ lib/counter_observe.dart
@@ -8,17 +8,18 @@ class CounterObserver extends BlocObserver {
     super.onChange(bloc, change);
     print(change);
   }
+
   @override
   void onTransition(
     Bloc<dynamic, dynamic> bloc,
     Transition<dynamic, dynamic> transition,
-  ){
+  ) {
     super.onTransition(bloc, transition);
     print(transition);
   }
 
   @override
-  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace){
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
     print(error);
     super.onError(bloc, error, stackTrace);
   }
