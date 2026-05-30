55c5f6e8f634c9ee73f397a6471244d497c07201
tika kartika <tikaputri469@gmail.com>
2026-05-30 07:50:54 +0700

    Create profile_page.dart

---
+++ lib/profile/view/profile_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter2/profile/view/profile_view.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ProfileView();
+  }
+}
