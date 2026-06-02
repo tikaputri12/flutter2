54d63817544559c4265323ee9a63808928e0b72c
tika kartika <tikaputri469@gmail.com>
2026-06-02 19:51:54 +0700

    Update profile_event.dart

--- lib/profile/bloc/profile_event.dart
+++ lib/profile/bloc/profile_event.dart
@@ -7,10 +7,27 @@ sealed class ProfileEvent extends Equatable {
   List<Object> get props => [];
 }
 
-final class LoadProfile extends ProfileEvent{
+final class LoadProfile extends ProfileEvent {
   final String token;
 
   const LoadProfile({required this.token});
-@override
+
+  @override
   List<Object> get props => [token];
+}
+
+final class UpdateProfileEvent extends ProfileEvent {
+  final String username;
+  final String email;
+
+  const UpdateProfileEvent({
+    required this.username,
+    required this.email,
+  });
+
+  @override
+  List<Object> get props => [
+        username,
+        email,
+      ];
 }
