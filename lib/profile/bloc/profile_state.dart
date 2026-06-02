c13d20b909837299b18e70d1ed8a0166d1024a53
tika kartika <tikaputri469@gmail.com>
2026-06-02 19:52:00 +0700

    Update profile_state.dart

--- lib/profile/bloc/profile_state.dart
+++ lib/profile/bloc/profile_state.dart
@@ -2,27 +2,38 @@ part of 'profile_bloc.dart';
 
 sealed class ProfileState extends Equatable {
   const ProfileState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class ProfileInitial extends ProfileState {}
+
 final class ProfileLoading extends ProfileState {}
+
 final class ProfileSuccess extends ProfileState {
   final ProfileModel profile;
 
- const ProfileSuccess({required this.profile});
+  const ProfileSuccess({
+    required this.profile,
+  });
 
- @override
- List<Object> get props => [profile];
+  @override
+  List<Object> get props => [profile];
 }
- 
 
-final class ProfileFailure  extends ProfileState {
+final class ProfileFailure extends ProfileState {
   final String message;
 
-  const ProfileFailure({required this.message});
-   @override
- List<Object> get props => [message];
+  const ProfileFailure({
+    required this.message,
+  });
+
+  @override
+  List<Object> get props => [message];
 }
+
+// update profile
+final class ProfileUpdating extends ProfileState {}
+
+final class ProfileUpdateSuccess extends ProfileState {}
