0504a07fab81b858709b037040bbc4bf6f57e9f4
tika kartika <tikaputri469@gmail.com>
2026-06-02 19:52:16 +0700

    Update profile_view.dart

--- lib/profile/view/profile_view.dart
+++ lib/profile/view/profile_view.dart
@@ -16,42 +16,48 @@ class _ProfileViewState extends State<ProfileView> {
 
   late int blocked = 0;
   bool get isBlocked => blocked == 1 ? true : false;
+
   final _formKey = GlobalKey<FormState>();
+
   final TextEditingController usernameController = TextEditingController();
+
   final TextEditingController emailController = TextEditingController();
 
-  //initstate
   @override
-  void initState(){
+  void initState() {
     super.initState();
- //load profile
- final token = context.read<AuthCubit>().state.token;
- context.read<ProfileBloc>().add(LoadProfile(token: token!));
+
+    // load profile
+    final token = context.read<AuthCubit>().state.token;
+
+    context.read<ProfileBloc>().add(LoadProfile(token: token!));
   }
+
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<ProfileBloc, ProfileState>(
       listener: (context, state) {
-        //jika state login success
-        if(state is ProfileSuccess){
+        // jika profile success
+        if (state is ProfileSuccess) {
           final profile = state.profile.data;
 
           usernameController.text = profile!.username;
           emailController.text = profile.email;
+
           confirmed = profile.confirmed;
           blocked = profile.blocked;
         }
 
-        //jika state login failure
-        if(state is ProfileFailure){
+        // jika profile gagal
+        if (state is ProfileFailure) {
           showDialog(
             context: context,
             builder: (context) => AlertDialog(
-              title: Text("Load Profile Failed"),
-              content: Text("$state.message"),
+              title: const Text("Load Profile Failed"),
+              content: Text(state.message),
               actions: [
                 TextButton(
-                  child: Text("OK"),
+                  child: const Text("OK"),
                   onPressed: () {
                     Navigator.of(context).pop();
                   },
@@ -61,24 +67,30 @@ class _ProfileViewState extends State<ProfileView> {
           );
         }
       },
+
       builder: (context, state) {
         return Scaffold(
-          appBar: AppBar(title: Text("Profile Page")),
+          appBar: AppBar(title: const Text("Profile Page")),
+
           body: SingleChildScrollView(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
+
             child: Column(
               children: [
                 buildProfileBuilder(),
-                SizedBox(height: 20),
 
-                //widget status card
+                const SizedBox(height: 20),
+
+                // status card
                 buildStatusCard(),
-                Divider(),
 
-                //form
+                const Divider(),
+
+                // form
                 buildForm(),
 
-                SizedBox(height: 32),
+                const SizedBox(height: 32),
+
                 buildLogoutButton(context),
               ],
             ),
@@ -92,15 +104,20 @@ class _ProfileViewState extends State<ProfileView> {
     return SizedBox(
       width: double.infinity,
       height: 50,
+
       child: OutlinedButton.icon(
         onPressed: () {
           Navigator.pop(context);
           Navigator.pushNamed(context, '/login');
         },
-        label: Text("Logout"),
+
+        label: const Text("Logout"),
+
+        icon: const Icon(Icons.logout),
+
         style: OutlinedButton.styleFrom(
           foregroundColor: Colors.teal,
-          side: BorderSide(color: Colors.teal),
+          side: const BorderSide(color: Colors.teal),
         ),
       ),
     );
@@ -109,54 +126,73 @@ class _ProfileViewState extends State<ProfileView> {
   Form buildForm() {
     return Form(
       key: _formKey,
+
       child: Column(
         children: [
           TextFormField(
             controller: usernameController,
+
             decoration: InputDecoration(
               labelText: 'Username',
-              prefixIcon: Icon(Icons.person),
+
+              prefixIcon: const Icon(Icons.person),
+
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(14),
               ),
             ),
+
             validator: (value) {
               if (value == null || value.isEmpty) {
                 return "Username wajib diisi";
               }
+
               return null;
             },
           ),
 
-          SizedBox(height: 8),
+          const SizedBox(height: 8),
 
           TextFormField(
             controller: emailController,
+
             decoration: InputDecoration(
-              labelText: 'email',
-              prefixIcon: Icon(Icons.email),
+              labelText: 'Email',
+
+              prefixIcon: const Icon(Icons.email),
+
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(14),
               ),
             ),
+
             validator: (value) {
               if (value == null || value.isEmpty) {
                 return "Email wajib diisi";
               }
+
               return null;
             },
           ),
 
-          SizedBox(height: 8),
+          const SizedBox(height: 8),
+
           SizedBox(
             width: double.infinity,
             height: 50,
+
             child: ElevatedButton.icon(
               onPressed: () {
-                print("Update profile ditekan");
+                Navigator.pop(context);
+
+                Navigator.pushNamed(context, '/update-profile');
+
+                print("update profile");
               },
-              label: Text("Update Profile"),
-              icon: Icon(Icons.save),
+
+              label: const Text("Update Profile"),
+
+              icon: const Icon(Icons.save),
             ),
           ),
         ],
@@ -167,20 +203,28 @@ class _ProfileViewState extends State<ProfileView> {
   Card buildStatusCard() {
     return Card(
       elevation: 2,
+
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+
       child: Padding(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
+
         child: Row(
           children: [
-            //icon
+            // icon
             Icon(
               isConfirmed ? Icons.verified : Icons.warning,
+
               color: isConfirmed ? Colors.green : Colors.red,
             ),
-            //text
+
+            const SizedBox(width: 10),
+
+            // text
             Expanded(
               child: Text(
                 isConfirmed ? "Account Verified" : "Account not verified",
+
                 style: TextStyle(
                   color: isConfirmed ? Colors.green : Colors.red,
                 ),
@@ -201,13 +245,18 @@ class _ProfileViewState extends State<ProfileView> {
   Column buildProfileBuilder() {
     return Column(
       children: [
-        CircleAvatar(radius: 50, child: Icon(Icons.person, size: 50)),
-        SizedBox(height: 12),
+        const CircleAvatar(radius: 50, child: Icon(Icons.person, size: 50)),
+
+        const SizedBox(height: 12),
+
         Text(
           usernameController.text,
-          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+
+          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
         ),
-        SizedBox(height: 4),
+
+        const SizedBox(height: 4),
+
         Text(emailController.text, style: TextStyle(color: Colors.grey[500])),
       ],
     );
