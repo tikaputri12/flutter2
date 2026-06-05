6791a9f336a18df96e406e0b6593947a32a36ef0
tika kartika <tikaputri469@gmail.com>
2026-06-05 19:07:53 +0700

    Create category_model.dart

---
+++ lib/categories/models/category_model.dart
@@ -0,0 +1,26 @@
+class CategoryModel {
+  final int id;
+  final String name;
+  final String description;
+
+  CategoryModel({
+    required this.id,
+    required this.name,
+    required this.description,
+  });
+
+  factory CategoryModel.fromJson(Map<String, dynamic> json) {
+    return CategoryModel(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "name": name,
+      "description": description,
+    };
+  }
+}
