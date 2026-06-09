e894bafbf21207cdb471eaf77a2d2aebc78c845d
tika kartika <tikaputri469@gmail.com>
2026-06-10 04:01:35 +0700

    Update theme_cubit.dart

--- lib/core/theme/theme_cubit.dart
+++ lib/core/theme/theme_cubit.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ThemeCubit extends Cubit<ThemeMode> {
   ThemeCubit() : super(ThemeMode.light);
+
   void toggleTheme() {
     emit(state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light);
   }
